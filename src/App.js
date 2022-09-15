3a9f958e2844ce71a740eb019c02f51ff5566e36
zubair2001 <zubair_mansuri2001@outlook.com>
2022-09-15 23:48:54 +0500

    Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import './App.css';
-import {Navbar,Feed,ChannelDetail,SearchFeed,VideoDetail} from './Components';
-import {BrowserRouter as Router,
-Route,
-Routes} from 'react-router-dom';
-import {Box} from '@mui/material';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from '@mui/material';
 
-function App() {
-  return (
-    <Router>
-      <Box sx={{backgroundColor: '#000'}}>
-        <Navbar/>
-        <Routes>
-          <Route path="/" exact element={<Feed/>}/>
-          <Route path="/video/:id" element={<ChannelDetail/>}/>
-          <Route path="/channel/:id" element={<VideoDetail/>}/>
-          <Route path="/search/:id" element={<SearchFeed/>}/>
-        </Routes>
-      </Box>
-    </Router>
-  );
-}
+import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
+
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
 
 export default App;
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,23 +1,36 @@
-import React from 'react'
-import {Stack} from '@mui/material'
-import {categories } from '../Utils/constants'
+import React from "react";
+import { Stack } from "@mui/material";
 
-// const selectedCategory='New';
-const Sidebar = ({selectedCategory,setSelectedCategory}) => {
-    return (
-        <Stack direction="row" sx={{overflowY:"auto",height:{sx:'auto',md:'95%'},flexDirection:{md:'column'},}}>
-            {categories.map((category)=>(
-                <button
-                className='category-btn'
-                onClick={()=>setSelectedCategory(category.name)}
-                style={{background:category.name===selectedCategory && '#FC1503',color:'white'}}
-                key={category.name}>
-                    <span style={{color:category.name===selectedCategory ? 'white':'red',marginRight:'15px'}}>{category.icon}</span>
-                    <span style={{opacity:category.name===selectedCategory ? '1' : '0.8'}}>{category.name}</span>
-                </button>
-            ))}
-        </Stack>
-    )
-}
+import { categories } from "../utils/constants";
 
-export default Sidebar
+const Categories = ({ selectedCategory, setSelectedCategory }) => (
+  <Stack
+    direction="row"
+    sx={{
+      overflowY: "auto",
+      height: { sx: "auto", md: "95%" },
+      flexDirection: { md: "column" },
+    }}
+  >
+    {categories.map((category) => (
+      <button
+        className="category-btn"
+        onClick={() => setSelectedCategory(category.name)}
+        style={{
+          background: category.name === selectedCategory && "#FC1503",
+          color: "white",
+        }}
+        key={category.name}
+      >
+        <span style={{ color: category.name === selectedCategory ? "white" : "red", marginRight: "15px" }}>
+          {category.icon}
+        </span>
+        <span style={{ opacity: category.name === selectedCategory ? "1" : "0.8" }}>
+          {category.name}
+        </span>
+      </button>
+    ))}
+  </Stack>
+);
+
+export default Categories;
