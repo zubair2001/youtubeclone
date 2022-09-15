24146f9c7e2cbaa49f56634de4d36ca49418532d
zubair2001 <zubair_mansuri2001@outlook.com>
2022-09-15 22:23:03 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
+import {Navbar,Feed,ChannelDetail,SearchFeed,VideoDetail} from './Components';
+import {BrowserRouter as Router,
+Route,
+Routes} from 'react-router-dom';
+import {Box} from '@mui/material';
 
 function App() {
   return (
-    <>
-
-    </>
+    <Router>
+      <Box sx={{backgroundColor: '#000'}}>
+        <Navbar/>
+        <Routes>
+          <Route path="/" exact element={<Feed/>}/>
+          <Route path="/video/:id" element={<ChannelDetail/>}/>
+          <Route path="/channel/:id" element={<VideoDetail/>}/>
+          <Route path="/search/:id" element={<SearchFeed/>}/>
+        </Routes>
+      </Box>
+    </Router>
   );
 }
 
